68c6d66ed6d0ed91b4bde6bfea2bcdf4753c428b
unknown <marcelly.cristini32@gmail.com>
2024-04-12 23:08:00 -0300

    atualização do código no arquivo cadastroEditorRevisor

--- front/src/components/Cadastro.tsx
+++ front/src/components/Cadastro.tsx
@@ -7,22 +7,20 @@ enum UserType {
 }
 
 export default function Cadastro (){
-    // Definindo os estados para o tipo de usuário, nome, email e senha
+
     const [userType, setUserType] = useState<UserType>(UserType.Editor);
     const [nome, setNome] = useState("");
     const [email, setEmail] = useState("");
     const [senha, setSenha] = useState("");
-  
-    // Função para lidar com o envio do formulário
+
     const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
       event.preventDefault();
-      // Exibindo os valores dos campos no console para fins de depuração
+
       console.log('Tipo:', userType);
       console.log('Nome:', nome);
       console.log('Email:', email);
       console.log('Senha:', senha);
 
-      // Limpando os campos do formulário após o envio
       setNome('');
       setEmail('');
       setSenha('');
@@ -34,19 +32,19 @@ export default function Cadastro (){
           <h2>Cadastro de Editor e Revisor</h2>
           <div className="form-group">
             <label htmlFor="option">Escolha o tipo de cadastro: </label>
-            {/* Select para escolher o tipo de usuário */}
+
             <select  
               className="input-field" 
               value={userType} 
-              onChange={(e) => setUserType(e.target.value as UserType)} // Atualiza o tipo de usuário com o valor selecionado
+              onChange={(e) => setUserType(e.target.value as UserType)} 
               required
             >
-              {/* Opções do select para Editor e Revisor */}
+
               <option value={UserType.Editor}>Editor</option>
               <option value={UserType.Revisor}>Revisor</option>
             </select>
           </div>
-          {/* Campos para o nome, email e senha */}
+
           <div className="form-group">
             <label htmlFor="name">Nome:</label>
             <input type="text" id="name" className="input-field" placeholder="Digite seu nome" value={nome} onChange={(e) => setNome(e.target.value)} required/>
@@ -59,7 +57,7 @@ export default function Cadastro (){
             <label htmlFor="password">Senha:</label>
             <input type="password" id="password" className="input-field" placeholder="Digite sua senha" value={senha} onChange={(e) => setSenha(e.target.value)} required/>
           </div>
-          {/* Botão para enviar o formulário */}
+
           <button type="submit" className="submit-button">Cadastrar</button>
         </form>
       </div>
